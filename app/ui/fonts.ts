b875d6b7f399fbb77db4f6ed60457ba9551f335d
Sohil-95 <sohilshaikh9564@gmail.com>
2025-02-22 18:36:36 +0530

    font added

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,10 @@
 import {Inter} from 'next/font/google'
 import { Lusitana } from 'next/font/google'
 
-const lusitanaFont = Lusitana({
+const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 })
 
-export const lusitana = lusitanaFont
+export default lusitana
 export const inter=Inter({subsets:['latin']})
